676a2ad12fb46c0e978d10d60ec97352c461ccd8
StandardGiraffe <dannyfekete@gmail.com>
2018-10-15 17:21:38 +0000

    Protected the Review controller behind a login wall.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,7 @@
 class ReviewsController < ApplicationController
 
+  before_action :require_login
+
   def create
     @review = Review.new(
       product_id: params[:product_id],
@@ -21,4 +23,12 @@ class ReviewsController < ApplicationController
     # params.require(:review).permit(:review => [:description, :rating])
   # end
 
+  def require_login
+    unless logged_in?
+      flash[:error] = "You must be logged in to access this section"
+      redirect_to product_path(params[:product_id])
+    end
+  end
+
 end
+
